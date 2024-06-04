631fd932e96d8de79dbfa0a150103bce7899ae23
Zagaz <andre_ranulfo@hotmail.com>
2024-06-04 09:50:33 -0300

    feat: Add ReviewDetail page component
    The ReviewDetail page component is added to the application. This component displays the details of a specific review, including the product ID and review ID.

---
+++ src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type ReviewDetailProps = {
+     params: {
+          productid: string;
+          reviewid: string;
+     };
+};
+
+
+
+export default function ReviewDetail(
+     { params }: ReviewDetailProps
+){
+     return (
+          <>
+               <h1>Review Detail</h1>
+               <p>Product ID: {params.productid}</p>
+               <p>Review ID: {params.reviewid}</p>
+          </>
+     )
+}
