8e8b321b56661980e893d6aea003571a08b046b1
Zagaz <andre_ranulfo@hotmail.com>
2024-06-05 17:17:24 -0300

    feat: Add introductory content to First and Second blog pages
    This commit adds introductory content to the "First" and "Second" blog pages. The content provides a brief description of the page's topic and sets the context for the readers. This enhancement improves the user experience by providing more informative and engaging blog posts.

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type BlogLayoutProps = {
+     children: React.ReactNode
+}
+
+export default function BlogLayout({children}: BlogLayoutProps) {
+
+     const style = {
+          margin: 0,
+          padding: '20px',
+          backgroundColor: 'lightgreen',
+     };
+     
+
+  return (
+     <>
+         <div style={style}> 
+    {children}
+    </div>
+    </>
+
+  )
+}
