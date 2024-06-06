ce4382e0c7d097e90fbde863a814844717db7552
Zagaz <andre_ranulfo@hotmail.com>
2024-06-06 15:50:30 -0300

    feat: Improve product details layout in ProductIdLayout component

--- src/app/products/[productid]/layout.tsx
+++ src/app/products/[productid]/layout.tsx
@@ -26,11 +26,13 @@ export default function ProductIdLayout({children}: Props) {
      return (
            <>
            <h3 style = {style}>Products Detailes</h3>
+          {/* The Product */}
            <div style = {{...card, flexDirection: 'column'}}>
            <img src='https://prd.place/200' />
            <h3>Product Name</h3>
            <h2>100,00</h2>
            </div>
+          
            {children}
            </>
           
