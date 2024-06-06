d75d21af679326ca1bfad036d517502b62d1eff6
Zagaz <andre_ranulfo@hotmail.com>
2024-06-06 15:49:30 -0300

    feat: Add ProductIdLayout component
    This commit adds the ProductIdLayout component to the application. The component is responsible for rendering the layout of the product details page, including the product image, name, and price. This enhancement improves the user experience by providing a visually appealing and informative product details page.

---
+++ src/app/products/[productid]/layout.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+type Props = {
+     children : React.ReactNode
+}
+export const metadata = {
+     title : 'Product Detailed',
+     descprition : "A short description of the product",
+}
+const style = {
+ backgroundColor : 'orange',
+}
+const card = {
+     display : 'flex',
+     flexDirection : 'column',
+     justifyContent : 'space-between',
+     padding : '1rem',
+     margin : '20px',
+     border : '1px solid #ccc',
+     borderRadius : '5px',
+     width : '300px',
+      
+}
+
+export default function ProductIdLayout({children}: Props) {
+     return (
+           <>
+           <h3 style = {style}>Products Detailes</h3>
+           <div style = {{...card, flexDirection: 'column'}}>
+           <img src='https://prd.place/200' />
+           <h3>Product Name</h3>
+           <h2>100,00</h2>
+           </div>
+           {children}
+           </>
+          
+     )
+}
