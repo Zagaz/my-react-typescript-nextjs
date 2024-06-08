e5e2060c3252b7d801a07c97732e00aeab75b500
Zagaz <andre_ranulfo@hotmail.com>
2024-06-07 22:06:40 -0300

    refactor: A comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Card from "@/components/Card";
 import ImageFull from "@/components/ImageCard";
 import Link from "next/link";
 
+// 100 posts
+
 export default function Home() {
      return (
           <>
                <main>
                     <h1>Home</h1>
                     <Card title="Card 1" id={1} />
+                    <Card title="Card 2" id={5} />
                </main>
           </>
      )
