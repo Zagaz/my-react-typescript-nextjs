24b250dea686ed575d88752be988ee04c4fa5f05
Zagaz <andre_ranulfo@hotmail.com>
2024-06-15 17:07:09 -0300

    refactor: Add comments data and route
    Add the comments data file and route to the project. This includes the creation of the `data.ts` file with an array of comments and the `route.ts` file with GET and POST functions for retrieving and adding comments. These changes enable the functionality to display and submit comments in the application.

---
+++ src/app/comments/data.ts
@@ -0,0 +1,18 @@
+import { text } from "stream/consumers";
+
+export const comments =[
+     {
+          id:1,
+          text: "Uno Est officia qui aute quis cillum ad aliqua non veniam nisi."
+     },
+     {
+          id:2,
+          text: "Duo Dolore adipisicing ad nostrud nulla eiusmod ut voluptate sunt ex excepteur elit."
+     }
+     ,
+     {
+          id:3,
+          text: "Tres Aute irure ullamco aliqua elit aliquip amet occaecat."
+     }
+
+]
